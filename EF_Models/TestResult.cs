43025040c00a322a87821964041ccfa9a986b842
Justin Ansardi <jba20783@email.vccs.edu>
2023-02-22 16:47:06 -0500

    Added EFcore project into solution.

---
+++ EF_Models/TestResult.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF.Models
+{
+    public class TestResult
+    {
+        public int TestResultID { get; set;}
+        public int SiteID  { get; set; }
+        public float? Temperature { get; set;}
+        public float? Ph { get; set; }
+        public decimal? Sality { get; set; }
+        public decimal? Alkalinty { get; set; }
+        public decimal? Calcium { get; set; }
+        public decimal? Magnesium { get; set; }
+        public decimal? Phosphate { get; set; }
+        public decimal? Nitrate { get; set; }
+        public decimal? Nitrite { get; set; }
+        public decimal? Ammonia { get; set; }
+
+        public Site? Site { get; set; } // may be redundant
+        public int UserID { get; set; }
+        public SiteUser? SiteUser { get; set; }
+        
+        
+
+
+
+    }
+}
