f902f337c0f3b940593e8bec0bfd72bc7e17c208
Justin Ansardi <jba20783@email.vccs.edu>
2023-02-24 12:35:57 -0500

    Added correct DI for MaelstormContext

--- Maelstrom/Program.cs
+++ Maelstrom/Program.cs
@@ -1,12 +1,15 @@
+global using EF_Models;
 using Maelstrom.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var ReynoldsConnectionString = builder.Configuration.GetConnectionString("Reynolds");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<MaelstromContext>(options =>
     options.UseSqlServer(ReynoldsConnectionString));
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -39,17 +42,17 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-      name: "areas",
-      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-    );
-});
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllerRoute(
+//      name: "areas",
+//      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+//    );
+//});
 
 app.MapRazorPages();
 
