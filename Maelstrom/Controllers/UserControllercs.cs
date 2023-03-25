1a642164649d205006a5b270a973007a20b6cbba
Justin Ansardi <jba20783@email.vccs.edu>
2023-03-25 09:47:25 -0400

    Adding Prototype User Page Logic

---
+++ Maelstrom/Controllers/UserControllercs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Maelstrom.Controllers
+{
+    public class UserControllercs : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Maelstrom/Pages/UserHome.cshtml.cs
+++ Maelstrom/Pages/UserHome.cshtml.cs
@@ -1,3 +1,4 @@
+using EF_Models.Models;
 using Maelstrom.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -5,8 +6,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Maelstrom
 {
-    
-    public class UserHomeModel : PageModel
+    /// <summary>
+    /// Test code for User Page
+    /// </summary>
+     public class UserHomeModel : PageModel
     {
         private readonly MaelstromContext _context;
 
@@ -17,6 +20,20 @@ namespace Maelstrom
         }
 
         public string Message { get; private set; } = "PageModel in C#";
+        
+        public AppUser? ThisAppUser { get; private set; }
+
+        public ICollection<Site>? ThisUsersSites { get; private set; }
+
+        public ICollection<TestResult>? ThisUsersTestResults  { get; private set; }
+
+
+        //This will need addititional logic for user to save fish to his personal fish collection. As opposed to just the Site "owning" it.
+        //public ICollection<Fish>? ThisUsersFish { get; private set; }
+
+
+        // Pick up on writting logic
+        // stopping here to check about razor page layouts
 
         public void OnGet()
         {
