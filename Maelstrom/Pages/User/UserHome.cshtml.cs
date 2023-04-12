23d7ddc5a37417c5f7579ae2697fed5cfac74c1b
Justin Ansardi <jba20783@email.vccs.edu>
2023-04-12 12:56:28 -0400

    Removed _context

--- Maelstrom/Pages/User/UserHome.cshtml.cs
+++ Maelstrom/Pages/User/UserHome.cshtml.cs
@@ -4,17 +4,16 @@ using Maelstrom.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-namespace Maelstrom
+
+namespace Maelstrom.Pages.User
 {
- 
+
     public class UserHomeModel : PageModel
     {
-        private readonly MaelstromContext _context;
         private readonly IAppUserService _appUserService;
-        public UserHomeModel(MaelstromContext context, IAppUserService appUserService)
+        public UserHomeModel( IAppUserService appUserService)
         {
-            _context = context;
-            _appUserService = appUserService;     
+            _appUserService = appUserService;
         }
 
         public string CurrentSiteType { get; set; } = string.Empty;
@@ -24,12 +23,12 @@ namespace Maelstrom
 
 
         [BindProperty(SupportsGet = true)]
-        public Site CurrentSite { get; private set; }   
+        public Site CurrentSite { get; private set; }
 
         // -- this feature is still in development --
         public string SiteImage { get; private set; }
 
-        public ICollection<TestResult>? CurrentSiteTestResults  { get; private set; } 
+        public ICollection<TestResult>? CurrentSiteTestResults { get; private set; }
 
         //This will need addititional logic for user to save fish to his personal fish collection. As opposed to just the Site "owning" it.
         //public ICollection<Fish>? ThisUsersFish { get; private set; }
@@ -37,11 +36,11 @@ namespace Maelstrom
         public void OnGet(Site currentSite)
         {
             //This block calls custom services to set this page's properities
-            CurrentAppUser = _appUserService.FindAppUser(User.Identity); 
+            CurrentAppUser = _appUserService.FindAppUser(User.Identity);
             CurrentUserSites = _appUserService.CurrentUserSites(CurrentAppUser);
             CurrentSite = _appUserService.SelectedSite(CurrentUserSites, currentSite);
             CurrentSiteTestResults = _appUserService.SelectedSiteTestResults(CurrentSite);
-            CurrentSiteType = _appUserService.GetSiteType(CurrentSite);   
+            CurrentSiteType = _appUserService.GetSiteType(CurrentSite);
         }
         public void OnPost()
         {
