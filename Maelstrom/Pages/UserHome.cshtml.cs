7a49f5a67ca5d38d6017b9c002790dca324bb9d4
Justin Ansardi <jba20783@email.vccs.edu>
2023-04-12 12:35:37 -0400

    removed unneccessary _context

--- Maelstrom/Pages/UserHome.cshtml.cs
+++ Maelstrom/Pages/UserHome.cshtml.cs
@@ -9,26 +9,21 @@ namespace Maelstrom
  
     public class UserHomeModel : PageModel
     {
-        private readonly MaelstromContext _context;
+       
         private readonly IAppUserService _appUserService;
-        public UserHomeModel(MaelstromContext context, IAppUserService appUserService)
-        {
-            _context = context;
+        public UserHomeModel(IAppUserService appUserService)
+        {   
             _appUserService = appUserService;     
         }
 
         public string CurrentSiteType { get; set; } = string.Empty;
         public AppUser? CurrentAppUser { get; private set; }
-
         public ICollection<Site>? CurrentUserSites { get; private set; }
-
-
         [BindProperty(SupportsGet = true)]
-        public Site CurrentSite { get; private set; }   
-
+        public Site CurrentSite { get; private set; }  
+        
         // -- this feature is still in development --
         public string SiteImage { get; private set; }
-
         public ICollection<TestResult>? CurrentSiteTestResults  { get; private set; } 
 
         //This will need addititional logic for user to save fish to his personal fish collection. As opposed to just the Site "owning" it.
@@ -36,7 +31,7 @@ namespace Maelstrom
 
         public void OnGet(Site currentSite)
         {
-            //This block calls custom services to set this page's properities
+            //This block calls custom services to set page properities
             CurrentAppUser = _appUserService.FindAppUser(User.Identity); 
             CurrentUserSites = _appUserService.CurrentUserSites(CurrentAppUser);
             CurrentSite = _appUserService.SelectedSite(CurrentUserSites, currentSite);
