cb317bac21ac7d4ec0899adce9bcacd7c1aa7ab8
Justin Ansardi <jba20783@email.vccs.edu>
2023-04-14 19:17:36 -0400

    In the middle of testing user logic

--- AppUserService/AppUserService.cs
+++ AppUserService/AppUserService.cs
@@ -2,6 +2,7 @@
 using EF_Models.Models;
 using EF_Models;
 using System.Security.Principal;
+using Microsoft.EntityFrameworkCore;
 
 namespace Maelstrom.Services
 {
@@ -32,6 +33,17 @@ namespace Maelstrom.Services
         {
             if(user.Email != "Default@Maelstrom.com")
             {
+                //var querySiteUsers = from SiteUser in _context.SiteUsers
+                //                     join Sites in _context.Sites on SiteUser.SiteID equals Sites.SiteID
+                //                     select new
+                //                     {
+                //                         site = SiteUser.Site,
+                //                         appUser = SiteUser.AppUser
+                //                     };
+
+                //var usersSites = querySiteUsers.Select(x => x).Where(x => x.appUser.Id == user.Id).Select(x => x.site).ToList();
+                //return usersSites;
+
                 var querySiteUsers = from SiteUser in _context.SiteUsers
                                      join Sites in _context.Sites on SiteUser.SiteID equals Sites.SiteID
                                      select new
@@ -39,7 +51,12 @@ namespace Maelstrom.Services
                                          site = SiteUser.Site,
                                          appUser = SiteUser.AppUser
                                      };
+
+               
                 var usersSites = querySiteUsers.Select(x => x).Where(x => x.appUser.Id == user.Id).Select(x => x.site).ToList();
+              
+
+               
                 return usersSites;
             }
 
@@ -80,6 +97,9 @@ namespace Maelstrom.Services
             var siteTypeQuery = _context.SiteTypes.Where(x => x.SiteTypeID == site.SiteTypeID).Select(x => x.Name);
             return siteTypeQuery.First();
         }
+
+      
+
 
     }
 }
--- Maelstrom/Areas/User/Pages/SiteManager/Index.cshtml.cs
+++ Maelstrom/Areas/User/Pages/SiteManager/Index.cshtml.cs
@@ -2,18 +2,36 @@ using EF_Models.Models;
 using Maelstrom.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Maelstrom.Areas.User.Pages.SiteManager
 {
     public class IndexModel : PageModel
     {
+        private readonly EF_Models.MaelstromContext _context;
         private readonly IAppUserService _appUserService;
-        public IndexModel(IAppUserService appUserService)
+        public IndexModel(IAppUserService appUserService,MaelstromContext context )
         {
+            _context = context;
             _appUserService = appUserService;
         }
+
+        
         public AppUser? CurrentAppUser { get; private set; }
         public ICollection<Site>? CurrentUserSites { get; private set; }
+        public ICollection<SiteType> AllSiteTypes { get; private set; }
+ 
+        public IList<Site> Site { get; set; } = default!;
+
+        //public async Task OnGetAsync()
+        //{
+        //    if (_context.Sites != null)
+        //    {
+        //       // cant figure this out right now
+        //    }
+        //}
+
+
         public void OnGet()
         {
             CurrentAppUser = _appUserService.FindAppUser(User.Identity);
