08a2a06cfb38d1bb89cb395b8bda089a2267128e
Justin Ansardi <jba20783@email.vccs.edu>
2023-04-15 12:51:10 -0400

    [Site types now viewable] Its been a long couple of days

--- Maelstrom/Areas/User/Pages/SiteManager/Index.cshtml.cs
+++ Maelstrom/Areas/User/Pages/SiteManager/Index.cshtml.cs
@@ -6,22 +6,31 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Maelstrom.Areas.User.Pages.SiteManager
 {
+    
+ 
+
+
     public class IndexModel : PageModel
     {
         private readonly EF_Models.MaelstromContext _context;
         private readonly IAppUserService _appUserService;
+
+        
         public IndexModel(IAppUserService appUserService, MaelstromContext context)
         {
             _context = context;
             _appUserService = appUserService;
         }
 
+        //public DisplayModel myModel { get; set; }
 
+        public ICollection<SiteType>? MySiteTypes { get; set; }
         public AppUser? CurrentAppUser { get; private set; }
 
         public IList<Site>? CurrentUserSites { get; private set; }
 
-        
+
+        public Dictionary<int, string>? myDictionary { get; set; } = new Dictionary<int, string> { };
 
     
 
@@ -29,9 +38,36 @@ namespace Maelstrom.Areas.User.Pages.SiteManager
         public void OnGet()
         {
             CurrentAppUser = _appUserService.FindAppUser(User.Identity);
-            CurrentUserSites = _appUserService.CurrentUserSites(CurrentAppUser).ToList();
-           
+            //CurrentUserSites = _appUserService.CurrentUserSites(CurrentAppUser).ToList();
+
+            var querySiteUsers = from SiteUser in _context.SiteUsers
+                                 join AppUser in _context.AppUsers on SiteUser.AppUser equals AppUser
+                                 join Sites in _context.Sites on SiteUser.SiteID equals Sites.SiteID
+                                 join SiteType in _context.SiteTypes on Sites.SiteType equals SiteType
+                                 where AppUser == CurrentAppUser
+                                 select new
+                                 {
+                                     siteUser = SiteUser,
+                                     sites = Sites,
+                                     appUser = AppUser,
+                                     siteType = SiteType
+
+                                 };
+            CurrentUserSites= querySiteUsers.Select(x => x.sites).ToList();
+
+            MySiteTypes = querySiteUsers.Select(x => x.siteType).ToList();
+            
+            foreach(var site in MySiteTypes)
+            {
+                myDictionary.Add(site.SiteTypeID, site.Name);
+            }
 
         }
     }
+
+    //public class DisplayModel
+    //{
+    //    public IList<Site>? Sites { get; set; }
+    //    public string SiteName { get; set; }
+    //}
 }
