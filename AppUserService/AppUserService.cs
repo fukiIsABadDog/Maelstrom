118bb441519a1707392318e01310968b5ebd4a2a
Justin Ansardi <jba20783@email.vccs.edu>
2023-04-16 08:49:19 -0400

    refactored code into Services

--- AppUserService/AppUserService.cs
+++ AppUserService/AppUserService.cs
@@ -9,29 +9,29 @@ namespace Maelstrom.Services
     /// <summary>
     /// This Service helps us reuse code accross multiple pages
     /// </summary>
-    public class AppUserService: IAppUserService
+    public class AppUserService : IAppUserService
     {
-        private readonly MaelstromContext _context;      
+        private readonly MaelstromContext _context;
         public AppUserService(MaelstromContext context)
         {
 
             _context = context;
         }
-       
+
         public AppUser FindAppUser(IIdentity user)
         {
             var currentAppUser = _context.Users.FirstOrDefault(x => x.Email == "Default@Maelstrom.com");
 
-            if(user.IsAuthenticated != false) 
+            if (user.IsAuthenticated != false)
             {
-                currentAppUser = _context.Users.FirstOrDefault(x => x.UserName == user.Name);    
+                currentAppUser = _context.Users.FirstOrDefault(x => x.UserName == user.Name);
             }
             return currentAppUser;
         }
 
         public ICollection<Site> CurrentUserSites(AppUser user)
         {
-            if(user.Email != "Default@Maelstrom.com")
+            if (user.Email != "Default@Maelstrom.com")
             {
                 //var querySiteUsers = from SiteUser in _context.SiteUsers
                 //                     join Sites in _context.Sites on SiteUser.SiteID equals Sites.SiteID
@@ -45,7 +45,7 @@ namespace Maelstrom.Services
                 //return usersSites;
 
 
-                var querySiteUsers = from SiteUser in _context.SiteUsers                                     
+                var querySiteUsers = from SiteUser in _context.SiteUsers
                                      join AppUser in _context.AppUsers on SiteUser.AppUser equals AppUser
                                      join Sites in _context.Sites on SiteUser.SiteID equals Sites.SiteID
                                      join SiteType in _context.SiteTypes on Sites.SiteType equals SiteType
@@ -58,9 +58,9 @@ namespace Maelstrom.Services
                                          siteType = SiteType
 
                                      };
-            
 
-                return querySiteUsers.Select( x => x.sites).ToList();
+
+                return querySiteUsers.Select(x => x.sites).ToList();
 
             }
 
@@ -74,24 +74,24 @@ namespace Maelstrom.Services
             }
         }
 
-       public Site SelectedSite(ICollection<Site> sites, Site currentSite)
+        public Site SelectedSite(ICollection<Site> sites, Site currentSite)
         {
 
             // could use opperator for oneline expression or cleaned up some other way
             // also,  need to use ID attribute instead after view is modified
 
-                if (currentSite.Name != null)
-                {
-                    return sites.First(x => x.Name == currentSite.Name);
-                }
-                else { return sites.FirstOrDefault(); }
+            if (currentSite.Name != null)
+            {
+                return sites.First(x => x.Name == currentSite.Name);
+            }
+            else { return sites.FirstOrDefault(); }
         }
 
         public ICollection<TestResult> SelectedSiteTestResults(Site site)
         {
             var currentSiteTestResultsQuery = _context.TestResults.Select(x => x).Where(x => x.SiteUser.SiteID == site.SiteID);
-            if(currentSiteTestResultsQuery.Any()) { return  currentSiteTestResultsQuery.ToList(); }
-            else { return new List<TestResult>(); }    
+            if (currentSiteTestResultsQuery.Any()) { return currentSiteTestResultsQuery.ToList(); }
+            else { return new List<TestResult>(); }
         }
 
         public string GetSiteType(Site site)
@@ -102,7 +102,38 @@ namespace Maelstrom.Services
             return siteTypeQuery.First();
         }
 
-      
+        public (ICollection<Site>, Dictionary<int, String>) CurrentUsersSitesAndTypes(AppUser user)
+        {
+
+            var querySiteUsers = from SiteUser in _context.SiteUsers
+                                 join AppUser in _context.AppUsers on SiteUser.AppUser equals AppUser
+                                 join Sites in _context.Sites on SiteUser.SiteID equals Sites.SiteID
+                                 join SiteType in _context.SiteTypes on Sites.SiteType equals SiteType
+                                 where AppUser == user
+                                 select new
+                                 {
+                                     siteUser = SiteUser,
+                                     sites = Sites,
+                                     appUser = AppUser,
+                                     siteType = SiteType
+
+                                 };
+
+            var currentUserSites = querySiteUsers.Select(x => x.sites).ToList();
+
+            var currentUserSiteTypes = querySiteUsers.Select(x => x.siteType).ToList();
+
+            var currentUserSiteTypesDict = new Dictionary<int, String>();
+
+            foreach (var site in currentUserSiteTypes)
+            {
+                currentUserSiteTypesDict.Add(site.SiteTypeID, site.Name);
+            }
+
+            return (currentUserSites, currentUserSiteTypesDict);
+        }
+
+
 
 
     }
--- Maelstrom/Areas/User/Pages/SiteManager/Index.cshtml.cs
+++ Maelstrom/Areas/User/Pages/SiteManager/Index.cshtml.cs
@@ -12,62 +12,23 @@ namespace Maelstrom.Areas.User.Pages.SiteManager
 
     public class IndexModel : PageModel
     {
-        private readonly EF_Models.MaelstromContext _context;
         private readonly IAppUserService _appUserService;
 
-        
-        public IndexModel(IAppUserService appUserService, MaelstromContext context)
-        {
-            _context = context;
+        public IndexModel(IAppUserService appUserService)
+        {  
             _appUserService = appUserService;
         }
-
-        //public DisplayModel myModel { get; set; }
-
-        public ICollection<SiteType>? MySiteTypes { get; set; }
         public AppUser? CurrentAppUser { get; private set; }
-
+        public ICollection<SiteType>? MySiteTypes { get; set; }
         public IList<Site>? CurrentUserSites { get; private set; }
-
-
-        public Dictionary<int, string>? myDictionary { get; set; } = new Dictionary<int, string> { };
-
-    
-
+        public Dictionary<int, string>? SiteTypeDictionary { get; set; } = new Dictionary<int, string> { };
 
         public void OnGet()
         {
             CurrentAppUser = _appUserService.FindAppUser(User.Identity);
-            //CurrentUserSites = _appUserService.CurrentUserSites(CurrentAppUser).ToList();
-
-            var querySiteUsers = from SiteUser in _context.SiteUsers
-                                 join AppUser in _context.AppUsers on SiteUser.AppUser equals AppUser
-                                 join Sites in _context.Sites on SiteUser.SiteID equals Sites.SiteID
-                                 join SiteType in _context.SiteTypes on Sites.SiteType equals SiteType
-                                 where AppUser == CurrentAppUser
-                                 select new
-                                 {
-                                     siteUser = SiteUser,
-                                     sites = Sites,
-                                     appUser = AppUser,
-                                     siteType = SiteType
-
-                                 };
-            CurrentUserSites= querySiteUsers.Select(x => x.sites).ToList();
-
-            MySiteTypes = querySiteUsers.Select(x => x.siteType).ToList();
-            
-            foreach(var site in MySiteTypes)
-            {
-                myDictionary.Add(site.SiteTypeID, site.Name);
-            }
-
+            var cusat = _appUserService.CurrentUsersSitesAndTypes(CurrentAppUser);
+            CurrentUserSites = cusat.Item1.ToList();
+            SiteTypeDictionary = cusat.Item2;
         }
-    }
-
-    //public class DisplayModel
-    //{
-    //    public IList<Site>? Sites { get; set; }
-    //    public string SiteName { get; set; }
-    //}
+    } 
 }
