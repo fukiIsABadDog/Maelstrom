e0f988135303fe761b857e5af3dc0510d0433447
Justin Ansardi <jba20783@email.vccs.edu>
2023-04-21 10:34:45 -0400

    TR delete page complete

--- Maelstrom/Areas/User/Pages/ResultManager/Delete.cshtml.cs
+++ Maelstrom/Areas/User/Pages/ResultManager/Delete.cshtml.cs
@@ -7,13 +7,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace Maelstrom.Areas.User.Pages.ResultManager
 {
 
-    // not complete! developing now 4/21
+
     [Authorize]
     public class DeleteModel : PageModel
     {
         private readonly EF_Models.MaelstromContext _context;
         private readonly IAppUserService _appUserService;
-
         public DeleteModel(EF_Models.MaelstromContext context, IAppUserService appUserService)
         {
             _context = context;
@@ -21,60 +20,53 @@ namespace Maelstrom.Areas.User.Pages.ResultManager
         }
 
         [BindProperty]
-        public TestResult TestResult { get; set; }
+        public TestResult TestResult { get; set; } = null!;
         [BindProperty]
         public SiteUser SiteUser { get; set; }
-        [BindProperty]
-        public int SiteID { get; set; }
         public AppUser AppUser { get; set; }
-
-
-        // If I have time. I will think about making a cookie for all this validation stuff.
+        // If I have time...I will research making a cookie for all this validation stuff.
         // I beleive it will improve performance... I really don't like going to the DB this much
-        public async Task<IActionResult> OnGetAsync(int? id)
+        // Also reconfigure onget to be async.. this might require async service methods...needs research too
+        public Task<IActionResult> OnGet(int? id)
         {
-
             if (id == null)
             {
-                return NotFound();
+                return Task.FromResult<IActionResult>(NotFound());
             }
             this.AppUser = _appUserService.FindAppUser(User.Identity);
             var testResult = _context.TestResults.Select(x => x).Where(x => x.TestResultID == id).FirstOrDefault();
             if (testResult == null)
             {
-                return NotFound();
+                return Task.FromResult<IActionResult>(NotFound());
             }
             var siteUser = _appUserService.CheckTestResultUser(AppUser, testResult);
-
             // need to impliment custom 404 page
             if (siteUser == null || testResult == null)
             {
-                return NotFound();
+                return Task.FromResult<IActionResult>(NotFound());
             }
             else
             {
                 SiteUser = siteUser;
                 TestResult = testResult;
             }
-            return Page();
+            return Task.FromResult<IActionResult>(Page());
         }
 
-        public async Task<IActionResult> OnPostAsync(int? id)
+        public async Task<IActionResult> OnPostAsync()
         {
-            if (id == null || _context.Sites == null)
+            if (TestResult == null || _context.TestResults == null)
             {
                 return NotFound();
             }
-            var tr = await _context.TestResults.FindAsync(id);
-
+            var tr = await _context.TestResults.FindAsync(TestResult.TestResultID);
             if (tr != null)
             {
-                _context.TestResults.Remove(tr);
+                TestResult = tr;
+                _context.TestResults.Remove(TestResult);
                 await _context.SaveChangesAsync();
             }
-
-            return RedirectToPage("/SiteManager/TestResults", new { id = SiteID.ToString() });
-
+            return RedirectToPage("/SiteManager/TestResults", new { id = SiteUser.SiteID.ToString() });
         }
     }
 }
