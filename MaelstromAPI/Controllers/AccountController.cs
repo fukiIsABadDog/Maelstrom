4d8e603beeeb7bbd0e632d03a90f7fbe4b40936d
Justin Ansardi <ansardi.justin@gmail.com>
2023-06-24 13:59:41 -0400

    Added Contructor

---
+++ MaelstromAPI/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using EF_Models;
+using EF_Models.Models;
+namespace Maelstrom.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly MaelstromContext _context;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(
+            MaelstromContext context,
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+    }
+}
