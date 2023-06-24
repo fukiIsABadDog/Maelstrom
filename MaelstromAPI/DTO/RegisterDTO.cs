00756667689dc386bd11fc9e35a6ab5298828fc1
Justin Ansardi <ansardi.justin@gmail.com>
2023-06-24 14:13:27 -0400

    Added Register DTO

---
+++ MaelstromAPI/DTO/RegisterDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Maelstrom.API.DTO
+{
+    public class RegisterDTO
+    {
+        [Required]
+        public string? FirstName { get; set; }
+        [Required] 
+        public string? LastName { get; set}
+        [Required]
+        [EmailAddress]
+        public string? EmailAddress { get; set; }
+        [Required]
+        public string? Password { get; set; }
+    }
+}
