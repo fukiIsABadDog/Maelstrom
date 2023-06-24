8b9a2c1a49a5dad3aec4bd7fa5701e7c3ef56887
Justin Ansardi <ansardi.justin@gmail.com>
2023-06-24 12:43:50 -0400

    Learning about Using Identity with API

--- MaelstromAPI/Controllers/SiteManager.cs
+++ MaelstromAPI/Controllers/SiteManager.cs
@@ -1,11 +1,35 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EF_Models.Models;
+using Maelstrom.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Principal;
+//using System.Web.Mvc;
 
-namespace MaelstromAPI.Controllers
+namespace Maelstrom.API.Controllers
 {
+    
     [Route("api/[controller]")]
     [ApiController]
     public class SiteManager : ControllerBase
     {
+        private readonly IAppUserService _appUserService;
+        public SiteManager(IAppUserService appUserService)
+        {
+            _appUserService = appUserService;
+        }
+
+        public IIdentity CurrentUser { get; private set; } = null!;
+        public IList<Site> CurrentUserSites { get; private set; } = null!;
+    
+
+        [HttpGet(Name = "MySites")]
+        public async Task<IList<Site>> OnGetAsync()
+        {
+            CurrentUser = User.Identity!; //This is going to require more configuration
+            var sites = (IList<Site>)await _appUserService.GetCurrentUserSites(CurrentUser);
+            CurrentUserSites = sites;
+
+            return CurrentUserSites.ToArray();
+        }
     }
 }
